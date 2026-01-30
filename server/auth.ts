4f0bcccb6ea55860658816743f089d5d5c4ff591
Diego Ambeliz <diegoambeliz@gmail.com>
2026-01-30 02:06:21 -0800

    feat: moved request ui directory

--- server/auth.ts
+++ server/auth.ts
@@ -5,7 +5,6 @@ export async function getDbUserOrThrow() {
   const { userId } = await auth();
   if (!userId) throw new Error("UNAUTHENTICATED");
 
-  console.log(userId);
   const dbUser = await prisma.user.findUnique({ where: { id: userId } });
   if (!dbUser) throw new Error("USER_NOT_SYNCED");
 
